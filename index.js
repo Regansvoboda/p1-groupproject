19efd267e2b0824e5542562f5ee0167e410bac8f
Mason Parks <kmparks60@gmail.com>
2023-02-13 17:12:50 -0500

    end of day 1

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 
 const musicAPI = "http://localhost:3000/Songs"
 const musicL = document.getElementById("music-list")
+const newCard = document.getElementById('new-song')
 
+newCard.addEventListener('submit', e=> {
+    e.preventDefault()
 
+const newMusicCard = {
+    title: e.target.title.value,
+    artist: e.target.artist.value,
+    album: e.target.album.value,
+    link: e.target.link.value
+}
 
+fetch(musicAPI, {
+    method: 'POST',
+    headers: {'Content-Type' : 'application/json'},
+    body: JSON.stringify(newMusicCard)
+})
+    .then(r => r.json())
+    .then(newMusicCard => {
+        renderMusicList(newMusicCard)
+    })
+})
 
 fetch(musicAPI)
     .then( r => r.json())
     .then(musicArray => {
         musicArray.forEach(musicObj => {
-            renderMusicList(musicObj)
+            console.log(musicObj)
             
         });
     })
 
+
 function renderMusicList(musicObj) {
     const musicDiv = document.createElement("div")
+    musicDiv.className = 'card'
     musicDiv.innerHTML = `
     <h2></h2>
     <h3></h3>
-    <img />
+    <img class='album-cover' />
     <p></p>
-    <button>Listen</button>`
+    <button class='listen-button'>Listen</button>`
 
     musicDiv.querySelector("h2").textContent = musicObj.artist
     musicDiv.querySelector("h3").textContent = musicObj.title
