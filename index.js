1c77eb09424106080ba73dbac8e292483ddf8f33
Kevin Duggan <kevinduggan95@gmail.com>
2023-02-13 12:08:05 -0600

    1st day lunch break

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//Comment test
+
+const musicAPI = "http://localhost:3000/Songs"
+const musicL = document.getElementById("music-list")
+
+
+
+
+fetch(musicAPI)
+    .then( r => r.json())
+    .then(musicArray => {
+        musicArray.forEach(musicObj => {
+            renderMusicList(musicObj)
+            
+        });
+    })
+
+function renderMusicList(musicObj) {
+    const musicDiv = document.createElement("div")
+    musicDiv.innerHTML = `
+    <h2></h2>
+    <h3></h3>
+    <img />
+    <p></p>
+    <button>Listen 🎧 </button>`
+
+    musicDiv.querySelector("h2").textContent = musicObj.artist
+    musicDiv.querySelector("h3").textContent = musicObj.title
+    musicDiv.querySelector("img").src = musicObj.album
+    
+    musicL.append(musicDiv)
+}
