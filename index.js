ea75787f4a3391657657436a8f8ed8549beaecb3
Regan Svoboda <regansvoboda@gmail.com>
2023-02-14 10:31:30 -0600

    event listener num 3

--- index.js
+++ index.js
@@ -11,52 +11,57 @@ newCard.addEventListener('submit', e=> {
         title: e.target.title.value,
         artist: e.target.artist.value,
         album: e.target.album.value,
+
     }
-    //button.addEventListener('mouseover', (e)=> {
-        //e.target.style.backgroundColor = "red"
-        //
-        
-        fetch(musicAPI, {
-            method: 'POST',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(newMusicCard)
-        })
-        .then(r => r.json())
-        .then(newMusicCard => {
-            renderMusicList(newMusicCard)
-        })
+    
+    fetch(musicAPI, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(newMusicCard)
     })
-    
-    fetch(musicAPI)
-    .then( r => r.json())
-    .then(musicArray => {
-        musicArray.forEach(musicObj => {
-            renderMusicList(musicObj)
-            
-        });
+    .then(r => r.json())
+    .then(newMusicCard => {
+        renderMusicList(newMusicCard)
     })
+})
+
+fetch(musicAPI)
+.then( r => r.json())
+.then(musicArray => {
+    musicArray.forEach(musicObj => {
+        renderMusicList(musicObj)
+    });
+})
+
+
+function renderMusicList(musicObj) {
+    const musicDiv = document.createElement("div")
+    musicDiv.className = 'card'
+    musicDiv.innerHTML = `
+    <h2></h2>
+    <h3></h3>
+    <img class='album-cover' />`
     
-    function renderMusicList(musicObj) {
-        const musicDiv = document.createElement("div")
-        musicDiv.className = 'card'
-        musicDiv.innerHTML = `
-        <h2></h2>
-        <h3></h3>
-        <img class='album-cover' />`
-        
-        // <button id='listen-button' class='listen-button'>Listen</button>
-       
-        musicDiv.querySelector("h2").textContent = musicObj.artist
-        musicDiv.querySelector("h3").textContent = musicObj.title
-        musicDiv.querySelector("img").src = musicObj.album
-        
-        musicL.append(musicDiv)
-    }
+    // <button id='listen-button' class='listen-button'>Listen</button>
     
-    const button = document.getElementById("create-button")
+    musicDiv.querySelector("h2").textContent = musicObj.artist
+    musicDiv.querySelector("h3").textContent = musicObj.title
+    musicDiv.querySelector("img").src = musicObj.album
     
-    button.addEventListener("click", showMessage) 
-    
-    function showMessage() {
-       alert("Thank you for submitting!")
-    }
+    musicL.append(musicDiv)
+}
+
+const button = document.getElementById("create-button")
+
+button.addEventListener("click", showMessage) 
+
+function showMessage() {
+    alert("Thank you for submitting!")
+}
+button.addEventListener('mouseover', (e)=> {
+    e.target.style.backgroundColor = "red"
+})
+
+button.addEventListener('mouseout', (e)=> {
+    e.target.style.backgroundColor = "white"
+})
